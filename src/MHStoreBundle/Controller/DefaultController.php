f14b955c7b6a7ae744802b684615e2570f1787cf
mathieuHa <mathieu.hanotaux@gmail.com>
2017-09-23 21:23:19 +0200

    Ajout des actions de base : - Ajout d'un produit - Visualisation d'un produit - Liste des Produits - Suppression d'un Produit
    Ajout d'un design pour tester les actions

--- src/MHStoreBundle/Controller/DefaultController.php
+++ src/MHStoreBundle/Controller/DefaultController.php
@@ -2,12 +2,79 @@
 
 namespace MHStoreBundle\Controller;
 
+use MHStoreBundle\Entity\Product;
+use MHStoreBundle\Form\ProductType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
+        return $this->render('MHStoreBundle:Default:index.html.twig');
+    }
+
+    public function addAction(Request $request)
+    {
+        $product = new Product();
+        $form = $this
+            ->get('form.factory')
+            ->create(ProductType::class, $product);
+
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($product);
+            $em->flush();
+
+            $request->getSession()->getFlashBag()->add('notice', 'Produit ajouté');
+
+            return $this->redirectToRoute('mh_store_view', array(
+                'id' => $product->getId()
+            ));
+        }
+
+        return $this->render('MHStoreBundle:Default:add.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+    public function viewAction($id)
+    {
+        $product = $this
+            ->getDoctrine()
+            ->getRepository('MHStoreBundle:Product')
+            ->find($id);
+
+        return $this->render('MHStoreBundle:Default:view.html.twig', array(
+            'product' => $product
+        ));
+    }
+
+    public function listAction()
+    {
+        $products = $this
+            ->getDoctrine()
+            ->getRepository('MHStoreBundle:Product')
+            ->findAll();
+
+        return $this->render('MHStoreBundle:Default:list.html.twig', array(
+            'products' => $products
+        ));
+    }
+
+    public function deleteAction($id)
+    {
+        $product = $this
+            ->getDoctrine()
+            ->getRepository('MHStoreBundle:Product')
+            ->find($id);
+        $em = $this
+            ->getDoctrine()
+            ->getManager();
+
+        $em->remove($product);
+        $em->flush();
+
         return $this->render('MHStoreBundle:Default:index.html.twig');
     }
 }
---
+++ src/MHStoreBundle/Form/ProductType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace MHStoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('price')->add('name')->add('description')->add('bid')
+            ->add('save', SubmitType::class);
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'MHStoreBundle\Entity\Product'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'mhstorebundle_product';
+    }
+
+
+}
