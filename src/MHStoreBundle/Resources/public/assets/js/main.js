f9b464ddc32978b1a7d261313bc0ae4eb40991e8
mathieuHa <mathieu.hanotaux@gmail.com>
2017-10-08 14:10:36 +0200

    improve design
    test enter key search
    
    Signed-off-by: mathieuHa <mathieu.hanotaux@gmail.com>

--- src/MHStoreBundle/Resources/public/assets/js/main.js
+++ src/MHStoreBundle/Resources/public/assets/js/main.js
@@ -15,4 +15,13 @@ $(function () {
         var url = Routing.generate('mh_store_search', { tag: word });
         window.location.href = url;
     });
+
+    $( "#searchText" ).keypress(function(e) {
+        if ( e.which == 13 ) {
+            e.preventDefault();
+            var word = $("#searchText").val();
+            var url = Routing.generate('mh_store_search', { tag: word });
+            window.location.href = url;
+        }
+    });
 });
