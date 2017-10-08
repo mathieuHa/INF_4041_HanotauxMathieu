163bf8664fca5d42c3ce0d25a79f511d2a677a93
mathieuHa <mathieu.hanotaux@gmail.com>
2017-10-08 13:59:00 +0200

    fix layout missing
    Add search with JSRouting bundle from any page of the app
    
    Add js / img / css folder properly installed
    
    Signed-off-by: mathieuHa <mathieu.hanotaux@gmail.com>

---
+++ src/MHStoreBundle/Resources/public/assets/js/main.js
@@ -0,0 +1,18 @@
+$(function () {
+    var jumboHeight = $('.jumbotron').outerHeight();
+    function parallax(){
+        var scrolled = $(window).scrollTop();
+        $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+    }
+
+    $(window).scroll(function(e){
+        parallax();
+    });
+
+
+    $("#searchButton").click(function () {
+        var word = $("#searchText").val();
+        var url = Routing.generate('mh_store_search', { tag: word });
+        window.location.href = url;
+    });
+});
